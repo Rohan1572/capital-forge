f70d5d65d737ee0fc250913a02139462945aaabd
Rohan Gupta <hellorohan02@gmail.com>
2026-09-09 22:38:23 +0530

    update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,40 @@
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintJs from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+export default defineConfig([
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,mts,cts,tsx,mtsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,mjsx}"],
+    extends: [eslintJs.configs.recommended],
+  },
+  {
+    files: ["**/*.{ts,mts,cts,tsx,mtsx}"],
+    extends: [
+      eslintJs.configs.recommended,
+      tseslint.configs.recommended,
+      eslintReact.configs["recommended-typescript"],
+    ],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    settings: {
+      "react-x": {
+        version: "19.2.3",
+      },
+    },
+  },
   prettierConfig,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
 ]);
-
-export default eslintConfig;
